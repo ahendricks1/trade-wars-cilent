187ca52e0dccf8ae27e01d32a10f18b1507bb1ae
Shayla Lane <lanesha@oregonstate.edu>
2019-11-17 14:57:10 -0800

    Refactor directory structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -38,7 +38,10 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
-
+	
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":3000", nil)
+	
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
