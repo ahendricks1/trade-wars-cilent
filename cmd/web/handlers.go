f66390e37b408a550c3c33fdcb79a6cdadcb3c9a
Shayla Lane <lanesha@oregonstate.edu>
2019-11-18 16:43:58 -0800

    Add footer handle to handlers file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,43 +1,44 @@
 package main
 
 import (
-   // "fmt"
-    "html/template" // New import
-    "log"           // New import
-    "net/http"
-    //"strconv"
+	// "fmt"
+	"html/template" // New import
+	"log"           // New import
+	"net/http"
+	//"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        http.NotFound(w, r)
-        return
-    }
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
-    files := []string{
-        "./ui/html/home.page.tmpl",
-        "./ui/html/base.layout.tmpl",
-    }
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
 
-    // Use the template.ParseFiles() function to read the template file into a
-    // template set. If there's an error, we log the detailed error message and use
-    // the http.Error() function to send a generic 500 Internal Server Error
-    // response to the user.
-    ts, err := template.ParseFiles(files...)
-    if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-        return
-    }
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and use
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
 
-    // We then use the Execute() method on the template set to write the template
-    // content as the response body. The last parameter to Execute() represents any
-    // dynamic data that we want to pass in, which for now we'll leave as nil.
-    err = ts.Execute(w, nil)
-    if err != nil {
-        log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
-    }
+	// We then use the Execute() method on the template set to write the template
+	// content as the response body. The last parameter to Execute() represents any
+	// dynamic data that we want to pass in, which for now we'll leave as nil.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func players(w http.ResponseWriter, r *http.Request) {
