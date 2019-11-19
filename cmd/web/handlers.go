5310b970bb0c55fb6a438d741c8e8823d0dd571b
Shayla Lane <lanesha@oregonstate.edu>
2019-11-19 10:32:40 -0800

    Set title to Trade Wars

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	//"fmt"
-	"html/template" // New import
-	"log"           // New import
+	"html/template" // functions that allow us to use templates
+	"log"           
 	"net/http"
 	//"strconv"
 )
