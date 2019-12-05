7d3f4216a4159a2ae57f26a43fd104f42ae0eef4
Shayla Lane <lanesha@oregonstate.edu>
2019-12-05 09:57:11 -0800

    Add javascript to move ship via keyboard

--- ui/static/js/style.js
+++ ui/static/js/style.js
@@ -23,3 +23,42 @@ function goDirection(button){
     }
 }
 
+	//init object globally
+	var objImage= null;
+	function init(){
+		objImage=document.getElementById("spaceship");				
+		objImage.style.position='relative';
+		objImage.style.left='0px';
+		objImage.style.top='0px';
+	}
+	function getKeyAndMove(e){				
+		var key_code=e.which||e.keyCode;
+		switch(key_code){
+			case 37: //left arrow key
+				moveLeft();
+				break;
+			case 38: //Up arrow key
+				moveUp();
+				break;
+			case 39: //right arrow key
+				moveRight();
+				break;
+			case 40: //down arrow key
+				moveDown();
+				break;						
+		}
+	}
+	function moveLeft(){
+		objImage.style.left=parseInt(objImage.style.left)-5 +'vw';
+	}
+	function moveUp(){
+		objImage.style.top=parseInt(objImage.style.top)-5 +'vw';
+	}
+	function moveRight(){
+		objImage.style.left=parseInt(objImage.style.left)+5 +'vw';
+	}
+	function moveDown(){
+		objImage.style.top=parseInt(objImage.style.top)+5 +'vw';
+	}
+	window.onload=init;
+
